825db21fe75837912160a4c6a0f6c4f3fa7e5788
Serban Nacu <serban.nacu@gmail.com>
2024-11-05 14:13:00 -0800

    Unit tests

---
+++ test/test_distribution.cpp
@@ -0,0 +1,109 @@
+#include "Distribution.h"
+
+#include "gtest/gtest.h"
+
+#include <cmath>
+
+namespace gxna {
+
+TEST(Distribution, BetaInc) {
+    // Special values
+    double eps = 1e-8;
+    for (double x = 0; x <= 1; x += 0.01) {
+        EXPECT_NEAR(betainc(1, 1, x), x, eps);
+        EXPECT_NEAR(betainc(2, 1, x), x * x, eps);
+        EXPECT_NEAR(betainc(4, 1, x), x * x * x * x, eps);
+        EXPECT_NEAR(betainc(1, 2, x), x * (2 - x), eps);
+    }
+}
+
+TEST(Distribution, zCDF) {
+    // Asymptotics
+    EXPECT_DOUBLE_EQ(zCDF(-40), 0);
+    EXPECT_NEAR(zCDF(-20), 0, 1e-80);
+    EXPECT_NEAR(zSF(20), 0, 1e-80);
+    EXPECT_DOUBLE_EQ(zSF(40), 0);
+
+    // Special values
+    double eps = 1e-9;
+    EXPECT_DOUBLE_EQ(zCDF(0), 0.5);
+    EXPECT_NEAR(zCDF(1), 0.841344746, eps);
+    EXPECT_NEAR(zCDF(2), 0.977249868, eps);
+    EXPECT_NEAR(zCDF(3), 0.998650102, eps);
+}
+
+TEST(Distribution, tCDF) {
+    // Asymptotics
+    for (double n = 0.2; n <= 10; ++n) {
+        EXPECT_NEAR(tCDF(-1e20, n), 0, 1e-4);
+        EXPECT_EQ(tCDF(0, n), 0.5);
+    }
+    
+    // Special values
+    constexpr double Pi = 3.141592653589793;
+    for (double t = -10; t <= 10; t += 0.2) {
+        EXPECT_NEAR(tCDF(t, 1), 0.5 + std::atan(t) / Pi, 1e-9);  // t(1) is Cauchy
+        EXPECT_NEAR(tCDF(t, 2), 0.5 + t / (2 * std::sqrt(2 + t * t)), 1e-9);
+        EXPECT_NEAR(tCDF(t, 1e6), zCDF(t), 1e-6);  // t(inf) is Normal
+    }
+}
+
+// F-distribution tail asymptotics
+double fSFApprox(double f, double n1, double n2) {
+    n1 /= 2;
+    n2 /= 2;
+    double beta = std::tgamma(n1) * std::tgamma(n2) / std::tgamma(n1 + n2);
+    double coeff = 1 / (beta * std::pow(n1 / n2, n2) * n2);
+    return std::pow(f, -n2) * coeff;
+}
+
+TEST(Distribution, fCDF) {
+    double eps = 1e-9;
+
+    for (double n1 = 0.5; n1 <= 10; n1 += 0.2)
+        for (double n2 = 0.5; n2 <= 10; n2 += 0.2) {
+            EXPECT_EQ(fCDF(0, n1, n2), 0);
+
+            // Asymptotics
+            for (auto f : { 1e4, 1e5, 1e6, 1e7 }) {
+                double val = fSF(f, n1, n2) / fSFApprox(f, n1, n2);
+                EXPECT_NEAR(val, 1, 100 / f) << f << ' ' << n1 << ' ' << n2;
+            }
+
+            // If X ~ F(m1, n2) then 1 / X ~ F(n2, n1)
+            for (auto x : { 0.3, 0.4, 0.5, 2.0, 3.0, 4.0 })
+                EXPECT_NEAR(fCDF(x, n1, n2), 1 - fCDF(1 / x, n2, n1), eps);
+        }
+
+    // Symmetry
+    for (double n = 0.1; n <= 10; n += 0.2)
+        EXPECT_NEAR(fCDF(1, n, n), 0.5, eps);
+
+    // Special values
+    EXPECT_NEAR(fCDF(3.0, 1, 1), 2 / 3., eps);
+    EXPECT_NEAR(fCDF(3.0, 2, 2), 3 / 4., eps);
+    EXPECT_NEAR(fCDF(5.0, 10, 20), .998903410, eps);
+}
+
+TEST(Distribution, T2Z) {
+    // T(Inf) is Normal
+    for (double t = -38; t <= 38; t += 1)
+        EXPECT_NEAR(t, t2z(t, 1e5), 0.005 * std::fabs(t));
+}
+
+TEST(Distribution, zCDFInv) {
+    // Symmetry
+    EXPECT_EQ(zCDFinv(0.5), 0);
+
+    // Special values (note absolute error is fairly high)
+    EXPECT_NEAR(zCDFinv(1e-316), -38.0278, 0.04);
+    EXPECT_NEAR(zCDFinv(1e-80), -18.9916, 0.04);
+    EXPECT_NEAR(zCDFinv(1e-40), -13.3109, 0.04);
+    EXPECT_NEAR(zCDFinv(1e-20), -9.2623, 0.015);
+    EXPECT_NEAR(zCDFinv(1e-10), -6.3613, 0.01);
+    EXPECT_NEAR(zCDFinv(0.01), -2.3263, 0.01);
+    EXPECT_NEAR(zCDFinv(0.025), -1.9599, 0.01);
+    EXPECT_NEAR(zCDFinv(0.8414), 1.0002, 0.0002);
+}
+
+}  // gxna
---
+++ test/test_statistics.cpp
@@ -0,0 +1,84 @@
+#include "Statistics.h"
+
+#include "gtest/gtest.h"
+
+#include <cmath>
+#include <vector>
+
+namespace gxna {
+
+class DataSetTest : public testing::Test {
+ protected:
+    DataSetTest()
+        : fa(a), fb(b), fc(c)
+    {}
+    
+    const std::vector<double> a { 1, 3, 2, 4, 5, 2.5, 0, 1, 0.8 };
+    const std::vector<double> b { 5, 3, 8, 8.1, 10, 7, 7, 9, 0.2, 4, 4, 4.5 };
+    const std::vector<double> c { 3, 2, 7, 3.1, 8, 9, 0, 0 };
+
+    FastDataSet fa;
+    FastDataSet fb;
+    FastDataSet fc;
+};
+
+TEST_F(DataSetTest, Basic) {
+    FastDataSet fz;
+    fz += fa;
+    fz += fb;
+    fz += fc;
+
+    EXPECT_TRUE(fz.size() == fa.size() + fb.size() + fc.size());
+
+    EXPECT_DOUBLE_EQ(fz.mean(), 4.179310344827587);
+    EXPECT_DOUBLE_EQ(fz.sd(), 3.0392126147209884);
+}
+
+TEST_F(DataSetTest, TF) {
+    double t = 3.7245441941478252;
+    EXPECT_DOUBLE_EQ(tstat(fa, fb), t);
+    EXPECT_DOUBLE_EQ(tstat(fb, fa), -t);
+
+    double tequal = 3.454821662116653;
+    EXPECT_DOUBLE_EQ(tstatEqual(fa, fb), tequal);
+    EXPECT_DOUBLE_EQ(tstatEqual(fb, fa), -tequal);
+
+    EXPECT_DOUBLE_EQ(fstat({fa, fb}), 11.935792717030486);
+    EXPECT_DOUBLE_EQ(fstat({fa, fb, fc}), 4.5688416461171313);
+}
+
+TEST(Gamma, Basic) {
+    constexpr double Pi = 3.141592653589793;
+    constexpr double Gamma = 0.577215664901532;
+    double eps = 1e-9;
+
+    // Spot checks
+    EXPECT_NEAR(harmonic(20), 3.5977396571436, eps);
+    EXPECT_NEAR(harmonic2(20), 1.59616324391302, eps);
+
+    // Special values
+    EXPECT_NEAR(digamma(0.5), -std::log(4) - Gamma, eps); 
+    EXPECT_NEAR(digamma(1), -Gamma, eps); 
+    EXPECT_NEAR(digamma(2), 1 - Gamma, eps);
+
+    // Spot checks
+    EXPECT_NEAR(digamma(10), 2.25175258906, eps);
+    EXPECT_NEAR(digamma(100), 4.60016185273, eps);
+
+    // Special values
+    EXPECT_NEAR(trigamma(0.5), Pi * Pi / 2, eps);
+    EXPECT_NEAR(trigamma(1), Pi * Pi / 6, eps);
+
+    // Spot checks
+    EXPECT_NEAR(trigamma(10), 0.105166335, eps);
+    EXPECT_NEAR(trigamma(100), 0.010050167, eps);
+}
+
+TEST(Gamma, Inverse) {
+    for (double x = 1e-10; x <= 1e10; x *= 2) {
+        double y = trigamma(x);
+        EXPECT_NEAR(x, trigammainv(y), 1e-6);
+    }
+}
+
+}  // gxna
