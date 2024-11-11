9189eab2b37279c8e4cfea85d609049029b82652
Serban Nacu <serban.nacu@gmail.com>
2024-11-11 10:33:43 -0800

    Add size checks and comments

--- include/MultipleTest.h
+++ include/MultipleTest.h
@@ -5,6 +5,7 @@
 #include "VectorUtil.h"
 
 #include <algorithm>
+#include <cassert>
 #include <limits>
 #include <vector>
 
@@ -12,10 +13,11 @@ namespace gxna {
 
 // Class for testing multiple hypotheses.
 // It computes raw and adjusted P-values using resampling (permutation tests).
+
 // The method ScoreCalculator::operator() is called for every new permutation
 // and must return a vector of scores.
-// Scores should be positive, so take the absolute value if using two-sided
-// statistics such as T.
+// Scores should be non-negative, so caller should take the absolute value
+// when using two-sided statistics such as T.
 // The first permutation generated MUST be id.
 
 // In some use cases, floating point errors in score calculation can result in
@@ -37,17 +39,21 @@ class MultipleTest {
     double getAdjP(int i) const { return m_adjP[i]; }
     double getRank(int i) const { return m_rank[i]; }
 
+    // Main entry point
     void test(PermutationGenerator& pg, bool scaled) {
+        // Compute raw and adjusted counts.
         if (scaled)
             maxTscaled(pg);
         else
             maxT(pg);
 
-        // finish computing P-values
-        for (int i = 1; i < m_nObjects; i++) {
-            if (m_adjP[i] < m_adjP[i-1])  // enforce monotonicity
+        // Enforce monotonicity for adjusted counts.
+        for (size_t i = 1; i < m_adjP.size(); ++i) {
+            if (m_adjP[i] < m_adjP[i-1])
                 m_adjP[i] = m_adjP[i-1];
         }
+
+        // Convert counts into P-values.
         m_rawP /= pg.count();
         m_adjP /= pg.count();
     }
@@ -55,8 +61,9 @@ class MultipleTest {
     void maxT(PermutationGenerator& pg) {
         while (pg.next()) {  // generate new permutation
             auto score = m_calc(pg.get());
+            assert(score.size() == m_nObjects);
             if (pg.count() == 1)  // first perm housekeeping
-                setRank(m_origT = score);
+                initRank(score);
             else
                 updateP(score);
         }
@@ -64,16 +71,19 @@ class MultipleTest {
 
     void maxTscaled(PermutationGenerator& pg) {
         std::vector< std::vector<double> > v_score;
-        std::vector<FastDataSet> v_data(m_nObjects);
+        std::vector<FastDataSet> v_data(m_nObjects);  // to compute mean and sd
 
-        while (pg.next()) {  // compute all scores for all perms
+        // Compute all scores for all permutations.
+        while (pg.next()) {
             auto score = m_calc(pg.get());
+            assert(score.size() == m_nObjects);
             v_score.emplace_back(score);
-            for (int i = 0; i < m_nObjects; ++i)
+            for (size_t i = 0; i < v_data.size(); ++i)
                 v_data[i].insert(score[i]);
         }
 
-        for (int i = 0; i < m_nObjects; ++i) {  // rescale scores
+        // Rescale each object score by its mean and sd across samples.
+        for (size_t i = 0; i < v_data.size(); ++i) {
             double mu = v_data[i].mean();
             double sigma = v_data[i].sd();
             for (auto& score : v_score) {
@@ -83,23 +93,32 @@ class MultipleTest {
             }
         }
 
-        setRank(m_origT = v_score[0]);
+        initRank(v_score[0]);
         for (size_t j = 1; j < v_score.size(); ++j)
             updateP(v_score[j]);
     }
 
  private:
-    void setRank(const std::vector<double>& score) {
-        m_rank.resize(m_nObjects);
-        for (int i = 0; i < m_nObjects; i++)
+
+    // Rank objects in decreasing order of their score.
+    // Use stable sort to get consistent behavior across platforms.
+
+    void initRank(const std::vector<double>& score) {
+        m_origT = score;
+        m_rank.resize(score.size());
+        for (size_t i = 0; i < m_rank.size(); ++i)
             m_rank[i] = i;
         std::stable_sort(m_rank.begin(), m_rank.end(),
                          [&](int j, int k) { return score[j] > score[k] + m_eps; });
     }
 
+    // Iterate over objects in increasing order of their original score.
+    // Compare resampled scores with original scores, update raw and adjusted
+    // counts as needed.
+
     void updateP(const std::vector<double>& score) {
         double currentMax = std::numeric_limits<double>::min();
-        for (int i = m_nObjects - 1; i >= 0; i--) {  // go thru all objects
+        for (int i = m_rank.size() - 1; i >= 0; --i) {  // go thru all objects
             int k = m_rank[i];
             double T = score[k];
             if (currentMax < T)
@@ -112,7 +131,7 @@ class MultipleTest {
     }
 
     ScoreCalculator& m_calc;
-    int m_nObjects;
+    size_t m_nObjects;
     double m_eps;
     std::vector<double> m_origT;
     std::vector<int> m_rank;
