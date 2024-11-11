c65eff9479bfa51c16eb600585d10605c3147905
Serban Nacu <serban.nacu@gmail.com>
2024-11-11 10:40:27 -0800

    Add scalar multiplication

--- include/VectorUtil.h
+++ include/VectorUtil.h
@@ -4,9 +4,9 @@
 
 namespace gxna {
 
-// Basic vector arithmetic
-// Not optimized, but good enough for our purposes, and avoids using third-party libraries
-// No size checks
+// Basic vector arithmetic.
+// Not optimized, but good enough for our needs, and avoids using third-party libraries.
+// No size checks.
 
 template<class T>
 std::vector<T>& operator+=(std::vector<T>& x, const std::vector<T>& y) {
@@ -23,13 +23,20 @@ std::vector<T>& operator-=(std::vector<T>& x, const std::vector<T>& y) {
 }
 
 template<class T, class U>
+std::vector<T>& operator*=(std::vector<T>& x, const U& y) {
+    for (size_t i = 0; i < x.size(); ++i)
+        x[i] *= y;
+    return x;
+}
+
+template<class T, class U>
 std::vector<T>& operator/=(std::vector<T>& x, const U& y) {
     for (size_t i = 0; i < x.size(); ++i)
         x[i] /= y;
     return x;
 }
 
-// Vector input/output
+// Vector input and output.
 
 template<typename T>
 std::istream& operator>>(std::istream& is, std::vector<T>& vec) {
